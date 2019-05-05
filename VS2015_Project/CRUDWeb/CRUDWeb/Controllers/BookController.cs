9b36296136dd7cbcbcb81b2e0f3db6422cba4069
kane <jericho20040@hotmail.com>
2019-05-05 22:17:07 +0800

    add BookController & GetBooks View

---
+++ VS2015_Project/CRUDWeb/CRUDWeb/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDWeb.Models;
+using CRUDWeb.DAL;
+
+namespace CRUDWeb.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult GetBooks()
+        {
+            Book b = new Book()
+            {
+                Id = Guid.NewGuid(),
+                Author = "kane",
+                Name = "mvc",
+                Page = 10,
+                Price = 100.0,
+                Publisher = "abc",
+                Created = DateTime.Now,
+                Description = "good book"
+            };
+
+            List<Book> books = new List<Book>();
+            books.Add(b);
+            return View(books);
+        }
+    }
+}
